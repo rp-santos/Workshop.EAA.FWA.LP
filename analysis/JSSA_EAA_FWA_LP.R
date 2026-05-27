c4b89cfbe1b558a1afb351e1ee45c38e83c514a9
Rui Peres dos Santos <rpsantos@pattern.institute>
2026-05-27 16:03:10 +0100

    update JSSA_EAA_FWA_LP

--- analysis/JSSA_EAA_FWA_LP.R
+++ analysis/JSSA_EAA_FWA_LP.R
@@ -1,65 +1,83 @@
 #This script shows a simplified workflow used to estimate fin whales size population using Jolly-Seber-Schwarz-Arnason capture-recapture model
 
-#Main workflow:
-#1. Load data
-#2. Create spatial grid
-#3. Build capture-recapture histories
-#4. Create habitat mask
-#5. Fit JSSA model
-
-
-remotes::install_github("rp-santos/Workshop.EAA.FWA.LP")
-remotes::install_github("patterninstitute/azores.fkw")
-
-library(tidyverse)
-library(patchwork)
-library(sf)
-library(secr)
-library(openCR)
-library(ggspatial)
-library(azores.fkw)
-library(Workshop.EAA.FWA.LP)
-
 #1. === LOAD DATA ===
 
-#1.1. BP INDIVIDUALS
-# Contains photo-identification detections of fin whales (all unique individuals
-# except the ones without valid geographic position)
-# Select only the Algarve region from Portugal to plot the data
-data("bp.individuals")
+#1.1. Include only the Algarve region
 data("exclude_regions")
+
 ggplot2::ggplot() +
-  ggplot2::geom_point(data = bp.individuals, ggplot2::aes(x = latitude, y = longitude), color = "blue", size = 0.5) +
+  ggplot2::geom_sf(data = exclude_regions, fill = NA, color = "black") +
   ggplot2::theme_minimal()
 
-#1.2.BOAT EFFORT
-#This step calculates survey effort for each grid cell and temporal sampling occasion
-#Cells were classified as ‘active’ if visited by the boat during a survey
-data("boat_trips_by_min")
+#1.2. BP INDIVIDUALS as capture data
+# Contains photo-identification detections of fin whales (all unique individuals
+# except the ones without valid geographic position)
+data("capture_data_sf")
 
-#1.3.CAPTHIST
+ggplot2::ggplot() +
+  ggplot2::geom_sf(data = capture_data_sf) +
+  ggplot2::geom_sf(data = exclude_regions, fill = NA, color = "black") +
+  ggplot2::theme_minimal()
+
+#1.3.BOAT EFFORT
+# This step calculates survey effort for each grid cell and temporal sampling occasion
+# Cells were classified as ‘active’ if visited by the boat during a survey
+data("effort_sf")
+
+ggplot2::ggplot() +
+  ggplot2::geom_sf(data = effort_sf) +
+  ggplot2::geom_sf(data = exclude_regions, fill = NA, color = "black") +
+  ggplot2::theme_minimal()
+
+#1.4. HABITAT MASK ===
+# It defines the area in which the animals are assumed to potentially occur and move
+data("habitat_mask")
+
+ggplot2::ggplot() +
+  ggplot2::geom_point(data = habitat_mask, ggplot2::aes(x = x, y = y), color = "blue", size = 0.5) +
+  ggplot2::geom_sf(data = exclude_regions, fill = NA, color = "black") +
+  ggplot2::theme_minimal()
+
+#1.5.CAPTHIST
 #It describes which individual was detected, where, when and under which survey effort
 data("caphist")
 
 
-#1.4. HABITAT MASK, CAPTURE DATA SF and EFFORT SF ===
-# It defines the area in which the animals are assumed to potentially occur and move
-data("habitat_mask")
-data("capture_data_sf")
-data("effort_sf")
-
-ggplot2::ggplot() +
-  ggplot2::geom_sf(data = exclude_regions, fill = NA, color = "black") +
-  ggplot2::geom_point(data = habitat_mask, ggplot2::aes(x = x, y = y), color = "blue", size = 0.5) +
-  ggplot2::theme_minimal() +
-  ggplot2::labs(title = "Mask + Faro District") +
-  ggplot2::geom_sf(data = capture_data_sf) +
-  ggplot2::geom_sf(data = effort_sf)
-
-#2. === JSSA MODEL Result===
+#1.6. === JSSA MODEL Result===
 # After comparing the three model (JSAAsecrb, JSAAsecrf, JSAAsecrl) using AIC, the best model is JSAAsecrb
 data("opencr_results_summarised")
 opencr_results_summarised
 
+#2. === JSSA MODEL Model for run ===
+set.seed(1)
+utm_resolution <- 1000
+buffer <- 10000
+trace <- TRUE
 
+JSSAsecrb <- openCR::openCR.fit(
+  caphist,
+  type = 'JSSAsecrb',
+  mask = habitat_mask,
+  trace = trace
+)
 
+# === Range in meters along easting (`x`) ===
+x_length <- diff(range(habitat_mask$x)) + utm_resolution
+# === Range in meters along northing (`y`) ===
+y_length <- diff(range(habitat_mask$y)) + utm_resolution
+# === Full area (including area of islands) in hectares ===
+full_area_ha <- x_length * y_length / utm_resolution
+# === Study area (excludes area of islands) in hectares ===
+study_area_ha <- (nrow(habitat_mask) * utm_resolution^2) / 10000
+# === Estimate density ===
+density_estimates_JSSAsecrb <- predict(JSSAsecrb, type = 'D')
+(superD_JSSAsecrb <- density_estimates_JSSAsecrb$superD * study_area_ha)
+
+# === Save results part of the results===
+opencr_results <- list(
+  model = JSSAsecrb,
+  density_estimates = density_estimates_JSSAsecrb,
+  superpopulation = superD_JSSAsecrb
+)
+
+opencr_results
